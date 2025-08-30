3d6a7fadb7b0689323c2dcb9d4b0845933362047
rchangy <rachel880925.chang@gmail.com>
2025-08-30 01:26:44 -0700

    init server code and attendence model

---
+++ server/model/attendance.js
@@ -0,0 +1,13 @@
+// Load required packages
+var mongoose = require('mongoose');
+
+// Define our user schema
+var TaskSchema = new mongoose.Schema({
+    uin: { type: String, required: true},
+    classId: { type: String, required: true},
+    date: { type: Date, required:true },
+    takenBy: { type: String, required: true}
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Attendance', AttendenceSchema);
